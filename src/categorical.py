ec6933083380fc83f6102d01a70dbb6d44c356fc
sivalokam <skkumarlokam@gmail.com>
2020-01-26 11:01:04 +0530

    added jupyter notebook

--- src/categorical.py
+++ src/categorical.py
@@ -1,9 +1,15 @@
-
+from sklearn import preprocessing
 class Categorical: 
 
     """
+    Categorical encoding
+    enc_type: label, binary, one_hot
+    
 
     """
 
-    def __init__(df,categorical_features,handle_na=False):
-        pass
+    def __init__(self,df,categorical_features,handle_na=False):
+        print("Initialization")
+
+if __name__=='__main__':
+    print("Hello Python")
